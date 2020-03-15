ed890c6478803700c2ef71085706a2b4e22cf4cd
zhangxuefeng <zhangxuefeng@ctrip.com>
2020-03-15 22:03:57 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './container/home/Home'
+
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div>
+       <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component }from 'react'
+import { Button } from 'antd';
+
+export default class Home extends Component {
+
+    render() {
+        return (
+          <div>
+            <Button type="primary">Primary</Button>
+          </div>
+        );
+      }
+}
